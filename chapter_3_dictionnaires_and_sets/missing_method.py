1a1b6ca41f89e80c5738aa9a493984add807d1e6
Guilherme Ferrari <guilhermefbravin@gmail.com>
2021-05-23 22:18:46 -0300

    add missing method for dict implementation

---
+++ chapter_3_dictionnaires_and_sets/missing_method.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+class StrDictKey(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError
+        return self[str(key)]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
+
+
+def test_access_dict_with_str_or_int():
+    str_dict = StrDictKey([("2", "two"), ("4", "four")])
+    assert str_dict[2] == str_dict["2"]
+    assert str_dict[4] == str_dict["4"]
+
+    with pytest.raises(KeyError):
+        _ = str_dict[1]
+
