ad87037e61eff651989c9fca8b1a35491b0e6b3e
Guilherme Ferrari <guilhermefbravin@gmail.com>
2021-05-23 21:51:58 -0300

    add default dict basic usage

---
+++ chapter_3_dictionnaires_and_sets/default_dict.py
@@ -0,0 +1,24 @@
+import collections
+
+
+def words_position(words: list[str]):
+    dd = collections.defaultdict(list)
+    for position, word in enumerate(words):
+        dd[word].append(position)
+
+    return dd
+
+
+def test_words_position():
+    test_words = "test phrase for counting repeated words for test for".split()
+    repeat_positions = words_position(test_words)
+
+    assert repeat_positions == {
+        "test": [0, 7],
+        "phrase": [1],
+        "for": [2, 6, 8],
+        "counting": [3],
+        "repeated": [4],
+        "words": [5],
+    }
+
